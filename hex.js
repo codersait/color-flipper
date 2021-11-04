ba6f3c88e410c7b740aec7a37343468e472362e3
codersait <codersait@gmail.com>
2021-11-04 10:05:13 +0300

    copy to clipboard button added

--- hex.js
+++ hex.js
@@ -1,5 +1,7 @@
 const color = document.querySelector('.color');
 const button = document.querySelector('.btn');
+const copyButton = document.getElementById('copy');
+copyButton.value = '#F1f5f8'
 
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
 
@@ -28,11 +30,22 @@ button.addEventListener('click', () => {
     document.body.style.background = hexValue;
     color.innerText = hexValue;
     button.style.background = hexButton;
+    copyButton.value = hexValue
     count++;
   } else {
     document.body.style.background = generateRGB();
-    color.innerText = generateRGB();
+    color.innerText = document.body.style.background;
+    copyButton.value = document.body.style.background;
     button.style.background = generateRGB();
     count++;
   }
 });
+
+copyButton.addEventListener('click', () => {
+  // copyButton.value = document.body.style.backgroundColor;
+  console.log(copyButton.value);
+  copyButton.select();
+  document.execCommand('copy')
+
+})
+
